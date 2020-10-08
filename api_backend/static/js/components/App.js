79446167c6fa36ffe03e1bd248fd56c225be2d01
RyanDajay <ryan.rdajay@gmail.com>
2020-10-09 00:14:51 +0800

    added scripts to package.json

--- api_backend/static/js/components/App.js
+++ api_backend/static/js/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-            <h1>React-Flask Integration</h1>
+            <h1>React-Flask Integration!!</h1>
         </div>
     )
 }
